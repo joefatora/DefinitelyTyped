275799175942c0e26073f01c9a1bff71b9242974
Joe Fatora <joe@altitudenetworks.com>
2018-10-25 11:15:12 -0700

    matched types to proptypes

--- types/react-bootstrap/lib/FormLabel.d.ts
+++ types/react-bootstrap/lib/FormLabel.d.ts
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { Sizes } from 'react-bootstrap';
 
 declare namespace FormLabel {
     export interface LabelProps extends React.HTMLProps<FormLabel> {
-        bsSize?: Sizes;
-        bsStyle?: string;
+        bsPrefix?: string;
+        srOnly?: boolean;
+        htmlFor?: string;
+        column?: boolean;
+        innerRef?: React.Ref<HTMLElement>
     }
 }
 declare class FormLabel extends React.Component<FormLabel.LabelProps> { }
